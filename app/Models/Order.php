350316935935e9baa3895a264826ef332734c26f
Irfan17101 <ramadhaniirfan868@gmail.com>
2025-07-12 23:33:43 +0700

    model order untuk pelanggan

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -9,12 +9,34 @@ class Order extends Model
 {
     use HasFactory;
 
-    protected $fillable = [
-        'nama',
-        'telepon',
-        'alamat',
-        'paket',
-        'berat',
-        'tgl_ambil',
+    /**
+     * The attributes that are not mass assignable.
+     * Menggunakan $guarded adalah alternatif dari $fillable.
+     * Dengan ['id'], semua kolom lain bisa diisi secara massal,
+     * termasuk 'pickup_time' dan kolom lain yang kita butuhkan.
+     * Ini adalah cara yang lebih fleksibel dan aman.
+     *
+     * @var array
+     */
+    protected $guarded = ['id'];
+
+    /**
+     * The attributes that should be cast to native types.
+     * Ini memastikan tipe data yang benar saat mengakses atribut.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'pickup_date' => 'date',
+        'total_price' => 'integer',
+        'weight' => 'decimal:2',
     ];
+
+    /**
+     * Mendefinisikan relasi: satu Order dimiliki oleh satu User.
+     */
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
